b8b6e251e016c5736ef85c4e88ef3e190887187c
Pedro Viegas <122245816+deluscc4@users.noreply.github.com>
2023-09-06 15:36:36 -0300

    Organizando Seus Ativos

--- desafio1/script_desafio3.swift
+++ desafio1/script_desafio3.swift
@@ -1,4 +1,4 @@
-*/
+/*
 Organizando Seus Ativos
 Descrição:
 Após uma análise cuidadosa realizada pela equipe de desenvolvimento de uma empresa bancaria, foi identificado a necessidade de uma nova funcionalidade para otimizar os processos e melhorias da experiência dos usuários. 
